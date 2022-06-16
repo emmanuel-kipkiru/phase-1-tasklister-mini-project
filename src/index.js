d0b1218e5d68dae6181acb08709ec6587d91cd36
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-16 19:48:27 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('sumbit',(e) => {
+    e.preventDefault()
+    makeTask(e.target['new-task-description'].value)
+    form.requestFullscreen()
+  })
 });
+function makeTask (task){
+  let li = document.createElement('li');
+  let button = document.createDocument('button')
+  button.addEventListener('click', deleteTask)
+  button.textContent = "Delete"
+  li.textContent = task +" "
+  li.appendChild(button)
+  document.getElementById("tasks").appendChild(li)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
